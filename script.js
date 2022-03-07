ce7c59c65c70a1d8af8d3fbe8430bbc42f99f027
PRAGATI KUMAR <34084017+pragatikumar@users.noreply.github.com>
2022-03-07 22:48:17 +0530

    First Web project

---
+++ script.js
@@ -0,0 +1,66 @@
+const data = {
+  mainTitle: "India City",
+  cardContents: [
+    {
+      name: "Gurugram",
+      imageLink:
+      "https://images.unsplash.com/photo-1514392181188-8f5d54262fa5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+
+    },
+    {
+      name: "Mumbai",
+      imageLink:
+      "https://images.unsplash.com/photo-1623250451650-fc629651e4d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+    },
+    {
+      name: "Bangalore",
+      imageLink:
+      "https://images.unsplash.com/photo-1600555179901-107bf80d25cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1866&q=80",
+    },
+    {
+      name: "Kolkata",
+      imageLink:
+      "https://images.unsplash.com/photo-1600713193398-7782a2874f5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=441&q=80",
+    },
+  ],
+  userData: { name: "Pragati Kumar", tagLine: "Baby steps into the world of web!" },
+};
+
+
+// Plug in title data to HTML
+let mainTitleElement = document.getElementById("main-title");
+mainTitleElement.innerHTML += data.mainTitle;
+
+
+// Create template for card content
+let getCardStructure = (name, imageLink) => {
+  return `<div class="col-12 col-sm-6 mb-4">
+  <div class="tile">
+    <div class="tile-text text-center">
+      <h5>${name}</h5>
+    </div>
+    <img 
+      src="${imageLink}" />
+  </div>
+</div>`;
+};
+
+let cardRow = document.getElementById("card-row");
+
+// Plug in card data to HTML
+data.cardContents.forEach((cardContent) => {
+  let { name, imageLink } = cardContent;
+  cardRow.innerHTML += getCardStructure(name, imageLink);
+});
+
+// Create template for footer content
+let getFooterStructure = (name, tagLine) => {
+  return `<h3 class="me-3"><i class="fas fa-heart"></i> ${name}</h3>
+    <p>${tagLine}</p>`;
+};
+
+let footerContent = document.getElementById("footer");
+let { name, tagLine } = data.userData;
+
+// Plug in footer data to HTML
+footerContent.innerHTML += getFooterStructure(name, tagLine);
